452f98bf197ccec5135968f74ef838acba2aa252
DianaWangui <dianawanguikt048@gmail.com>
2023-12-12 14:06:38 +0300

    ading comments

--- readinginfo.c
+++ readinginfo.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 int main(void)
 {
-  int number;
   char *data;
   FILE *ptr;
+  size_t size;
   ptr = fopen("writing.txt", "r");
   if (ptr == NULL)
   {
     printf("Error opening file!\n");
     exit(EXIT_FAILURE);
   }
+  /* Allocating memory dynamically for the buffer to store the data read*/
   data = (char *)malloc(20);
-  if (data == NULL)
+  if (data == NULL) /* checking condition where malloc fails and exit*/
   {
     perror("Failed to allocate memeory\n");
     exit(EXIT_FAILURE);
   }
-  if (fgets(data, 100, ptr) == NULL)
+  if (fgets(data, 20, ptr) == NULL) /* Reading all characters using fgets() upto to the size allocated*/
   {
     perror("Failed to read data\n");
   }
-  printf("DATA READ: %s", data);
-  fclose(ptr);
+  printf("DATA READ: %s", data); /* Printing the data read to the stdout*/
+  free(data);                    /* free the allocated memory after use*/
+  fclose(ptr);                   /* close the file*/
   return (0);
 }
