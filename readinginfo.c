8ee9c8135014a88df5c152e90f2bbe7549a1062f
DianaWangui <dianawanguikt048@gmail.com>
2023-12-12 14:30:55 +0300

    reading from a file

--- readinginfo.c
+++ readinginfo.c
@@ -21,6 +21,7 @@ int main(void)
   if (fgets(data, 20, ptr) == NULL) /* Reading all characters using fgets() upto to the size allocated, if null print error*/
   {
     perror("Failed to read data\n");
+    exit(EXIT_FAILURE);
   }
   printf("DATA READ: %s", data); /* Printing the data read to the stdout after success reading using fgets()*/
   free(data);                    /* free the allocated memory after use*/
