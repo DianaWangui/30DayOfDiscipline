7e5e19b3769a233c2a39330ca15d0742e2d64a73
DianaWangui <dianawanguikt048@gmail.com>
2023-12-13 22:48:54 +0300

    update

--- task1.c
+++ task1.c
@@ -27,8 +27,10 @@ int main(void)
   while (read = getline(&str, &len, fptr) != -1)
   {
     printf("%s", str);
+    /* checking number of characters read */
     read_char += read;
   }
+  /* %zu is the specifier to print charater read using getline()*/
   printf("\n%zu", read_char);
   /* using ferror() to check whether an error has occurred on when reading data in the file*/
   if (ferror(fptr))
