70f7313212b07c63d5fc42058a89fd6704664998
DianaWangui <dianawanguikt048@gmail.com>
2023-12-17 23:52:41 +0300

    reading all lines in the file after appending

--- python/write_read.py
+++ python/write_read.py
@@ -1,28 +1,21 @@
 """Opening a file in write and read mode"""
-
 with open("diana.txt", "w+") as file:
   
   """Writing to the file"""
-
   file.write("Hello, Diana! I hope you are doing well.")
 
   """Moving the pointer of the file object to the beginning of the file"""
-
   file.seek(0)
   
   """print what is in the file"""
-
   print(file.read())
 
-  """Adding some more content"""
-
-  file.write("\nHow are you?")
-  
-  """The file is closed automatically when leaving the with block"""
+  """Adding some more content using append mode"""
+  with open("diana.txt", "a") as file2:
+    file2.write("\nHow are you?")
 
 
-  """Opening a file without using the with statement
-  This way we need to manually close the file at the end"""
+  """Printing all content in the file"""
   with open("diana.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
