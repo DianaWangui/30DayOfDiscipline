0dbfbfbe985b32592103d55b60d660c0808fb001
DianaWangui <dianawanguikt048@gmail.com>
2023-12-18 20:12:11 +0300

    writing into dict.csv

---
+++ python/write_dict.py
@@ -0,0 +1,15 @@
+import csv
+def writeDict():
+  with open('dict.csv', 'w') as f:
+    fieldname = ['Name', 'dept', 'birth_month']
+    writer = csv.DictWriter(f, fieldnames=fieldname)
+
+    """Adding data into the file"""
+    writer.writeheader()
+    writer.writerow({'Name': 'Diana', 'dept': 'IT', 'birth_month': 'Aug'})
+    writer.writerow({'Name': 'John', 'dept': 'Sales', 'birth_month': 'Nov'})
+    writer.writerow({'Name': 'Kelvin', 'dept': 'Finance', 'birth_month': 'Dec'})
+
+  print("Finished writing data")
+if __name__ == "__main__":
+  writeDict()
