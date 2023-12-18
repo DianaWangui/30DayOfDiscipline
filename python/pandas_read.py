4d8eedbfc2a63d97e3d3cf9a8cb3b8c8d24bc554
DianaWangui <dianawanguikt048@gmail.com>
2023-12-18 19:47:19 +0300

    correcting pandas import

--- python/pandas_read.py
+++ python/pandas_read.py
@@ -1,4 +1,7 @@
-import pandas as pd
+# this csv import should be pandas module
+#was getting an error of module not found
+#so i changed it temp to cvs
+import csv as pd
 def pandas():
   df = pd.read_csv("hrdata.csv")
   print(df)
