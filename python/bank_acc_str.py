1146ed9fe943cf452b18426033bd96227667c4b8
DianaWangui <dianawanguikt048@gmail.com>
2023-12-27 13:02:57 +0300

    using str magical method to print the class methods of the bank account

---
+++ python/bank_acc_str.py
@@ -0,0 +1,24 @@
+class BankAccount:
+  def __init__(self, holder_name, balance=0):
+    self.holder_name = holder_name
+    self.balance = balance
+
+  def deposit(self, amount):
+    self.balance += amount
+    print(f"You have deposited {amount} to your account.")
+
+  def withdraw(self, amount):
+    if amount <= self.balance:
+      self.balance -= amount
+      print(f"You have withdrawn {amount} from your account.")
+    else:
+      print(f"Insufficient funds your balance is {self.balance}")
+
+  def __str__(self):
+    return f"Bank Account of {self.holder_name} with initial balance {self.balance}\n"
+  
+obj = BankAccount("Diana", 200)
+print(obj)
+obj.deposit(500)
+obj.withdraw(1000)
+print(obj)
