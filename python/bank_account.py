6d9c85b63778d3aa99b0188ca98604b223d26120
DianaWangui <dianawanguikt048@gmail.com>
2023-12-27 12:32:03 +0300

    a simple bank account with holder name and account balance with withdraw and deposit methods

--- python/bank_account.py
+++ python/bank_account.py
@@ -1,4 +1,4 @@
-class Bank:
+class BankAccount:
   """Instantiate the holder name and balace to 0"""
   def __init__(self, holder_name, balance=0):
     self.holder_name = holder_name
@@ -9,20 +9,23 @@ class Bank:
     print(f"Hello {self.holder_name}. Your account balance is {self.balance}")
     self.deposit = deposit
     self.balance += self.deposit
-    print(f"You have deposited {self.deposit}. Your new balance is {self.balance}\n")
+    print(f"You have deposited {self.deposit}.")
+    print(f"Your new balance is {self.balance}\n")
 
   # Withdrawal method stating amount to withdraw, cannot withdraw with less than balance
   def withdraw(self, withdraw):
     print(f"Hello {self.holder_name}")
     self.withdraw = withdraw
     if self.withdraw > self.balance:
-      print(f"You do not have enough fund to withdraw {self.withdraw}. Your balance is {self.balance}\n")
+      print(f"You do not have enough fund to withdraw {self.withdraw}.")
+      print(f"Your balance is {self.balance}\n")
     else:
       self.balance -= self.withdraw
-      print(f"You have withdrawn {self.withdraw}. Your new balance is {self.balance}\n")
+      print(f"You have withdrawn {self.withdraw}.")
+      print(f"Your new balance is {self.balance}\n")
 
 # testing by calling the methods
-b = Bank("Diana", 200)
+b = BankAccount("Diana", 200)
 b.withdraw(500)
 b.deposit(1000)
 b.balance
