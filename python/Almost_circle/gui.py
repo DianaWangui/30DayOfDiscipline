4ceb15e481df3e898c7d991a6a06ebf4e8646f43
DianaWangui <dianawanguikt048@gmail.com>
2023-12-29 23:15:36 +0300

    update

--- python/Almost_circle/gui.py
+++ python/Almost_circle/gui.py
@@ -34,10 +34,7 @@ button.place(x=300, y=0)
 
 #listbox
 frame1=Frame(root, bd=3, width=700, height=280, bg="#32405b")
-frame1.pack(pady=(160, 0))
-
-listbox= Listbox(frame1, font=('aria', 12), width=40, height=16, bg="#32405b")
-listbox.pack(side=LEFT, fill=BOTH, padx=2)
+frame1.pack(pady=(160,0))
 
 
 root.mainloop()
