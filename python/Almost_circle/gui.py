042596d6b68107abca4ee9dfab3582b79aedcd3c
DianaWangui <dianawanguikt048@gmail.com>
2023-12-29 23:33:46 +0300

    update

--- python/Almost_circle/gui.py
+++ python/Almost_circle/gui.py
@@ -32,9 +32,16 @@ task_entry.focus()
 button=Button(frame, text="ADD", font="arial 20 bold", width=6, bg="#5a95ff", fg="#fff", bd=0)
 button.place(x=300, y=0)
 
+
 #listbox
-frame1=Frame(root, bd=3, width=700, height=280, bg="#32405b")
-frame1.pack(pady=(160,0))
+frame1= Frame(root, bd=3,width=700,height=200,bg="#32405b")
+frame1.pack(pady=(230, 0))
 
+listbox = Listbox(frame1,font=('arial',12),width=40,height=16,bg="#32405b", fg="white", cursor="hand2", selectbackground="#5a95ff")
+listbox.pack(side=LEFT , fill=BOTH, padx=2)
+scrollbar= Scrollbar(frame1)
+scrollbar.pack(side=RIGHT, fill=BOTH)
 
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
 root.mainloop()
